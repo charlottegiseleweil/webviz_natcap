95c83cc2be8d4e3251e924c1a8d2fe8d1d666e81
Charlotte Gisele Weil <weilcharlotte@gmail.com>
2017-02-10 16:08:58 -0800

    reorder parcoords axis

--- data_treatment_scripts/tana_pretreatment.py
+++ data_treatment_scripts/tana_pretreatment.py
@@ -40,15 +40,15 @@ df.index.name = "index"
 new_cols={'obj_1_wt':'awy_weight','obj_2_wt':'sde_weight','obj_3_wt':'sdl_weight','AWY_1_agg_delta_abs':'awy_score','SDE_2_agg_delta_abs':'sde_score','SDL_3_agg_delta_abs':'sdl_score', 'sdr_1':'input_1','awy_1':'input_2', 'cat_scen':'input_spat', 'budget':'input_budget'}
 df.rename(columns=new_cols, inplace=True)
 
-#Reorder columns
-df_reordered = df[['awy_weight','sde_weight', 'sdl_weight','awy_score', 'sde_score', 'sdl_score','esm_case','input_budget', 'input_2','input_1','input_spat', 'AWY_1_rast_delta_abs', 'SDE_2_rast_delta_abs','SDL_3_rast_delta_abs']]
-
 #Divide by 10^6 all obj scores
 for i in ['sde_score','awy_score','sdl_score']:
 	df[i] = df[i]/1e6
 
+#Reorder columns
+df_reordered = df[['awy_weight','sde_weight', 'sdl_weight','awy_score','sde_score', 'sdl_score','esm_case','input_budget', 'input_2','input_1','input_spat', 'AWY_1_rast_delta_abs', 'SDE_2_rast_delta_abs','SDL_3_rast_delta_abs']]
+
 #check
-print(df.columns)
+print(df_reordered.columns)
 
 #Write cleaned file
-df.to_csv('../data/MaraguaFeb9.csv')
+df_reordered.to_csv('../data/MaraguaFeb9.csv')
