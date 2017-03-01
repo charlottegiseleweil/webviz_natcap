9cfc2aeb991312b94b155f8ecf5d411c8d32a683
Charlotte Gisele Weil <weilcharlotte@gmail.com>
2017-03-01 11:09:32 -0800

    Clean new dataset + add + remove columns_piechart from table + crossfilter only on wanted columns

--- js/crossfiltering.js
+++ js/crossfiltering.js
@@ -3,8 +3,10 @@ function crossfiltering(){
     /* Crossfilter constructor*/
     cf = crossfilter(full_data);
 
+
     /*Dimensions*/
-    Object.keys(full_data[0]).forEach(function(column_name) {
+    //all columns = Object.keys(full_data[0])
+    columnsToCrossfilter.forEach(function(column_name) {
         dimensions[column_name] = cf.dimension(function(d){
             return d[column_name];
         });
